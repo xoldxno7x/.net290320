54e97fdffa715afddfaf751c8864fb6dd0c7957c
xoldxno7x <36238563+xoldxno7x@users.noreply.github.com>
2020-06-16 18:29:06 +0300

    Add files via upload

---
+++ Computer/Computer/Computer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Computer
+{
+    [System.Diagnostics.DebuggerDisplay("Model = {model}, Price = {price*3.67}")]
+    class Computer
+    {
+        public string model;
+        public int price;
+        public int numberOfProcessors;
+        public float screenSize;
+        public bool isTurnOn = false;
+
+        public int TellMeThePrice()
+        {
+            return this.price;
+        }
+
+        public float TellMeTheScreenSize()
+        {
+            return this.screenSize;
+        }
+
+        public void TurnOn()
+        {
+            this.isTurnOn = true;
+        }
+        public void TurnOff()
+        {
+            this.isTurnOn = false;
+        }
+
+        public void AddProcessor()
+        {
+            this.numberOfProcessors++;
+        }
+
+        public override string ToString()
+        {
+            return $"Computer: Model - {model}, Price - {price}, NumberOfProcessors - {numberOfProcessors}, ScreenSize - {screenSize}, isTurnOn - {isTurnOn}";
+        }
+    }
+}
---
+++ Computer/Computer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Computer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer asus = new Computer {
+                model = "VivoBook 16",
+                price = 5000,
+                numberOfProcessors = 4,
+                screenSize = 16
+            };
+
+            Computer lenovo = new Computer
+            {
+                model = "SmartBook",
+                price = 6400,
+                numberOfProcessors = 8,
+                screenSize = 13
+            };
+
+            Computer lenovoLegion = new Computer
+            {
+                model = "Legion",
+                price = 15000,
+                numberOfProcessors = 16,
+                screenSize = 20
+            };
+
+            Console.WriteLine(asus.TellMeThePrice());
+            Console.WriteLine(asus.TellMeTheScreenSize());
+            asus.TurnOn();
+            Console.WriteLine(asus.isTurnOn);
+            asus.AddProcessor();
+            Console.WriteLine(asus.numberOfProcessors);
+            Console.WriteLine(asus.ToString());
+
+            Console.WriteLine(lenovo.TellMeThePrice());
+            Console.WriteLine(lenovo.TellMeTheScreenSize());
+            lenovo.TurnOn();
+            Console.WriteLine(lenovo.isTurnOn);
+            lenovo.AddProcessor();
+            Console.WriteLine(lenovo.numberOfProcessors);
+            Console.WriteLine(lenovo.ToString());
+
+
+            Console.WriteLine(lenovoLegion.TellMeThePrice());
+            Console.WriteLine(lenovoLegion.TellMeTheScreenSize());
+            lenovoLegion.TurnOn();
+            Console.WriteLine(lenovoLegion.isTurnOn);
+            lenovoLegion.AddProcessor();
+            Console.WriteLine(lenovoLegion.numberOfProcessors);
+            Console.WriteLine(lenovoLegion);
+        }
+    }
+}
