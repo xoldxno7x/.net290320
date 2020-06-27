43f1a37384553447e7c338384dbff3a9a2c8f701
xoldxno7x <36238563+xoldxno7x@users.noreply.github.com>
2020-06-27 13:35:45 +0300

    Add files via upload

---
+++ HW_6_28/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_6_28
+{
+    class Game
+    {
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+
+        public Game()
+        {
+        }
+
+        public Game(string name, float rating = (float)5.5)
+        {
+            _name = name;
+            _rating = rating;
+        }
+
+        public Game(string name, int numberOfPlayers, float rating, string originCountry)
+        {
+            _name = name;
+            _numberOfPlayers = numberOfPlayers;
+            _rating = rating;
+            _originCountry = originCountry;
+        }
+
+        public int TellMeHowManyPlayers()
+        {
+            return _numberOfPlayers;
+        }
+
+        public override string ToString()
+        {
+            return $"Game: name is {_name}, number of players is {_numberOfPlayers}, rating is {_rating}, origin country is {_originCountry}";
+        }
+    }
+}
---
+++ HW_6_28/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace HW_6_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Game witcher = new Game("Witcher 3", 1, (float)5.5, "Poland");
+
+            //2
+            Game fallOut = new Game()
+            {
+                _name = "Fall Out 3",
+                _originCountry = "USA",
+                _numberOfPlayers = 1,
+                _rating = 5
+            };
+
+            //3 
+            Game doom = new Game("Doom Internal")
+            {
+                _numberOfPlayers = 1,
+                _originCountry = "USA",
+                _rating = 6
+            };
+
+            //4
+            Game darkSouls = new Game("Dark Souls 3", (float)4.5)
+            {
+                _numberOfPlayers = 1,
+                _originCountry = "Japan"
+            };
+
+            //Testing 
+            PrintGame(witcher);
+            PrintGame(fallOut);
+            PrintGame(doom);
+            PrintGame(darkSouls);
+        }
+
+        //Will have an S next to it in ILDASM
+        public static void PrintGame(Game g)
+        {
+            int number = g.TellMeHowManyPlayers();
+            Console.WriteLine(g);
+        }
+
+        //Will not have an S next to it in ILDASM
+        public void notStatic()
+        {
+
+        }
+    }
+}
