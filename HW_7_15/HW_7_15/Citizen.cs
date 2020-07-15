3c82474d97cab7e1754632b9d411a4b80eab47ac
xoldxno7x <36238563+xoldxno7x@users.noreply.github.com>
2020-07-15 13:25:46 +0300

    Add files via upload

---
+++ HW_7_15/HW_7_15/Citizen.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_7_15
+{
+    class Citizen
+    {
+        public string _name;
+        public Citizen[] _children;
+        public readonly int _fatherID;
+        public readonly int _id;
+        public static int _numberOfCurrentCitizens;
+        public const int MAXIMUM_NUMBER_OF_CITIZENS = 1000000;
+
+        public Citizen(string name, int fatherID)
+        {
+            _name = name;
+            _fatherID = fatherID;
+            _numberOfCurrentCitizens++;
+            _id = _numberOfCurrentCitizens + fatherID;
+            _children = new Citizen[0];
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine(_numberOfCurrentCitizens);
+        }
+
+        public static bool ReachedHalfOfMaximumSize()
+        {
+            return _numberOfCurrentCitizens >= MAXIMUM_NUMBER_OF_CITIZENS;
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine(_id);
+        }
+
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            Console.WriteLine(Math.Abs(_fatherID-_id));
+        }
+
+        public void SetChildren(Citizen[] arr)
+        {
+            _children = arr;
+        }
+
+        //Helper method for ToString
+        private string PrintChildren()
+        {
+
+            if(_children != null && _children.Length != 0)
+            {
+                string s = "children: [ ";
+
+                foreach (Citizen c in _children)
+                {
+                    s += c.ToString() + " ";
+                }
+
+                return s + " ]";
+            }
+
+            return "no children";
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Citizen: name is {_name}, {PrintChildren()}, father ID is {_fatherID}, id is {_id}, number of current citizens is {_numberOfCurrentCitizens}, maximum number of citizens is {MAXIMUM_NUMBER_OF_CITIZENS}";
+        }
+    }
+}
---
+++ HW_7_15/HW_7_15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW_7_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen Kane = new Citizen("Kane", 1);
+
+            Citizen Kid = new Citizen("John", Kane._id);
+            Citizen Kid1 = new Citizen("Jane", Kane._id);
+            Citizen Kid2 = new Citizen("James", Kane._id);
+
+            Citizen[] sonsOfKane = new Citizen[] { Kid, Kid1, Kid2 };
+            Kane.SetChildren(sonsOfKane);
+
+            Console.WriteLine(Kane);
+            Console.WriteLine(HasChildren(Kane));
+        }
+
+        public static bool HasChildren(Citizen c)
+        {
+            return c._children != null && c._children.Length > 0;
+        }
+
+        //According to what I understood, this method should check whether the childs ID is the same as the Parent ID
+        public static bool CheckValidity(Citizen c)
+        {
+            if (HasChildren(c))
+            {
+                foreach(Citizen i in c._children)
+                {
+                    if(i._id == c._id)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
