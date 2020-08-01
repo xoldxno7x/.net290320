92c6602360d8c4397154f3dd9ae6d7f174a65608
xoldxno7x <36238563+xoldxno7x@users.noreply.github.com>
2020-08-01 12:30:38 +0300

    Add files via upload

---
+++ Hotel/Clients/Guest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clients
+{
+    public class Guest
+    {
+        private int _id;
+        private string _name;
+
+        //not assigning id value as default is 0, assigining value is redundant
+        public Guest(int id,string name)
+        {
+            _id = id;
+            _name = name;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public override string ToString()
+        {
+            return $"Guest [ id is: {_id}, name is: {_name} ]";
+        }
+    }
+}
---
+++ Hotel/Hotel/FamilyRoom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel
+{
+    class FamilyRoom : Room
+    {
+        public FamilyRoom(int roomNumber) : base(roomNumber)
+        {
+            _price = 100;
+            
+        }
+
+        protected override void SetFeatrues()
+        {
+            _features = "TV, minibar, king size bed";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
